da65b87fcd3d0e2166b0cf7ee8c9ef53931c3f7e
Scott Kellman <scottkellman@hotmail.com>
2020-07-31 13:11:25 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace GPSExifForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.ButtonSource = new System.Windows.Forms.Button();
+            this.textBoxSource = new System.Windows.Forms.TextBox();
+            this.textBoxDestination = new System.Windows.Forms.TextBox();
+            this.buttonDestination = new System.Windows.Forms.Button();
+            this.buttonProcess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ButtonSource
+            // 
+            this.ButtonSource.Location = new System.Drawing.Point(12, 24);
+            this.ButtonSource.Name = "ButtonSource";
+            this.ButtonSource.Size = new System.Drawing.Size(75, 23);
+            this.ButtonSource.TabIndex = 0;
+            this.ButtonSource.Text = "Source";
+            this.ButtonSource.UseVisualStyleBackColor = true;
+            this.ButtonSource.Click += new System.EventHandler(this.ButtonSource_Click);
+            // 
+            // textBoxSource
+            // 
+            this.textBoxSource.Location = new System.Drawing.Point(93, 27);
+            this.textBoxSource.Name = "textBoxSource";
+            this.textBoxSource.Size = new System.Drawing.Size(634, 20);
+            this.textBoxSource.TabIndex = 1;
+            // 
+            // textBoxDestination
+            // 
+            this.textBoxDestination.Location = new System.Drawing.Point(93, 56);
+            this.textBoxDestination.Name = "textBoxDestination";
+            this.textBoxDestination.Size = new System.Drawing.Size(634, 20);
+            this.textBoxDestination.TabIndex = 3;
+            // 
+            // buttonDestination
+            // 
+            this.buttonDestination.Location = new System.Drawing.Point(12, 53);
+            this.buttonDestination.Name = "buttonDestination";
+            this.buttonDestination.Size = new System.Drawing.Size(75, 23);
+            this.buttonDestination.TabIndex = 2;
+            this.buttonDestination.Text = "Destination";
+            this.buttonDestination.UseVisualStyleBackColor = true;
+            this.buttonDestination.Click += new System.EventHandler(this.buttonDestination_Click);
+            // 
+            // buttonProcess
+            // 
+            this.buttonProcess.Location = new System.Drawing.Point(12, 97);
+            this.buttonProcess.Name = "buttonProcess";
+            this.buttonProcess.Size = new System.Drawing.Size(75, 23);
+            this.buttonProcess.TabIndex = 4;
+            this.buttonProcess.Text = "Process";
+            this.buttonProcess.UseVisualStyleBackColor = true;
+            this.buttonProcess.Click += new System.EventHandler(this.buttonProcess_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonProcess);
+            this.Controls.Add(this.textBoxDestination);
+            this.Controls.Add(this.buttonDestination);
+            this.Controls.Add(this.textBoxSource);
+            this.Controls.Add(this.ButtonSource);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button ButtonSource;
+        private System.Windows.Forms.TextBox textBoxSource;
+        private System.Windows.Forms.TextBox textBoxDestination;
+        private System.Windows.Forms.Button buttonDestination;
+        private System.Windows.Forms.Button buttonProcess;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,211 @@
+﻿using LevDan.Exif;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace GPSExifForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSource_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                textBoxSource.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void buttonDestination_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                textBoxDestination.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void buttonProcess_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            double latitude = 0;
+            double longitude = 0;
+            string altitude = string.Empty;
+            string dateTime = string.Empty;
+            string make = string.Empty;
+            string model = string.Empty;
+            List<string> photos = new List<string>();
+            DirectoryInfo source = new DirectoryInfo(textBoxSource.Text);
+            DirectoryInfo destination = new DirectoryInfo(textBoxDestination.Text);
+
+            foreach (FileInfo path in source.GetFiles("*", SearchOption.AllDirectories))
+            {
+                sb.Append(Path.GetFileName(path.FullName));
+
+                try
+                {
+                    ExifGPSLatLonTagCollection exif = new ExifGPSLatLonTagCollection(path.FullName);
+
+                    if (exif.Count() >= 3)//datetime, lat, long
+                    {
+                        foreach (ExifTag tag in exif)
+                        {
+                            string latRef = string.Empty;
+                            string lonRef = string.Empty;
+
+                            foreach (ExifTag tag2 in exif)
+                            {
+                                switch (tag2.FieldName)
+                                {
+                                    case "GPSLatitudeRef":
+                                        {
+                                            latRef = tag2.Value;
+                                            break;
+                                        }
+                                    case "GPSLongitudeRef":
+                                        {
+                                            lonRef = tag2.Value;
+                                            break;
+                                        }
+                                }
+                            }
+                            switch (tag.FieldName)
+                            {
+                                case "GPSLatitude":
+                                    {
+                                        if (!string.IsNullOrEmpty(latRef))
+                                        {
+                                            latitude = Utilities.GPS.GetLatLonFromDMS(latRef.Substring(0, 1) + tag.Value);
+                                        }
+                                        latitude = Utilities.GPS.GetLatLonFromDMS(tag.Value);
+                                        break;
+                                    }
+                                case "GPSLongitude":
+                                    {
+                                        if (!string.IsNullOrEmpty(lonRef))
+                                        {
+                                            longitude = Utilities.GPS.GetLatLonFromDMS(lonRef.Substring(0, 1) + tag.Value);
+                                        }
+                                        longitude = Utilities.GPS.GetLatLonFromDMS(tag.Value);
+                                        break;
+                                    }
+                                case "GPSAltitude":
+                                    {
+                                        altitude = tag.Value;
+                                        break;
+                                    }
+                                case "DateTime":
+                                    {
+                                        dateTime = tag.Value;
+                                        break;
+                                    }
+                                case "Make":
+                                    {
+                                        make = tag.Value;
+                                        break;
+                                    }
+                                case "Model":
+                                    {
+                                        model = tag.Value;
+                                        break;
+                                    }
+                            }
+                        }
+                        sb.Append(Path.GetFileName(path.FullName) + "," + latitude + "," + longitude + "," + altitude + "," + make + "," + model);
+                        sb.Append(Environment.NewLine);
+                        photos.Add(longitude + "," + latitude + "," + altitude + "," + dateTime + "," + Path.GetFileName(path.FullName) + "," + make + "," + model);
+                        latitude = 0;
+                        longitude = 0;
+                        altitude = string.Empty;
+                        dateTime = string.Empty;
+                        make = string.Empty;
+                    }
+                    else
+                    {
+                        sb.Clear();
+                    }
+                }
+                catch (Exception) { }
+            }
+
+            if (photos.Count > 0)
+            {
+                string path3 = Path.Combine(@"C:\Users\OCONUS4\Desktop\Cases\Results", "photos.kml");
+                KML.Create(photos, path3);
+
+                string path2 = Path.Combine(@"C:\Users\OCONUS4\Desktop\Cases\Results", "report.txt");
+                File.WriteAllText(path2, sb.ToString());
+            }
+            else
+            {
+                MessageBox.Show("No locational data in the selected photo set.");
+            }
+
+            MessageBox.Show("Done processing files");
+        }
+        public static class KML
+        {
+            public static void Create(List<string> points, string path)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                using (XmlWriter writer = XmlWriter.Create(path))
+                {
+                    writer.WriteStartElement("Document");
+                    writer.WriteElementString("name", "photos.xml");
+                    writer.WriteElementString("open", "1");
+
+                    writer.WriteStartElement("Style");
+                    writer.WriteStartElement("LabelStyle");
+                    writer.WriteElementString("color", "ff0000cc");
+                    writer.WriteEndElement();//LabelStyle
+                    writer.WriteEndElement();//Style
+
+                    foreach (string p in points)
+                    {
+                        string[] splitUp = p.Split(',');
+                        sb.Append("Latitude: " + splitUp[0].ToString());
+                        sb.Append(Environment.NewLine);
+                        sb.Append("Longitude: " + splitUp[1].ToString());
+                        sb.Append(Environment.NewLine);
+                        sb.Append("Altitude: " + splitUp[2].ToString());
+                        sb.Append(Environment.NewLine);
+                        sb.Append("Date Time: " + splitUp[3].ToString());
+                        sb.Append(Environment.NewLine);
+                        sb.Append("File Name: " + splitUp[4].ToString());
+                        sb.Append(Environment.NewLine);
+                        sb.Append("Make: " + splitUp[5].ToString());
+                        sb.Append(Environment.NewLine);
+                        sb.Append("Model: " + splitUp[6].ToString());
+
+                        if (splitUp[0].ToString() != "0" && splitUp[1].ToString() != "0")
+                        {
+                            writer.WriteStartElement("Placemark");
+                            writer.WriteElementString("description", sb.ToString());
+                            writer.WriteElementString("name", splitUp[4].ToString());
+                            writer.WriteStartElement("Point");
+                            writer.WriteElementString("coordinates", splitUp[0].ToString() + "," + splitUp[1].ToString());
+                            writer.WriteEndElement();//Point
+                            writer.WriteEndElement();//Placemark
+                        }
+                        sb.Clear();
+                    }
+
+                    writer.WriteEndElement();//Document
+                    writer.Flush();
+                }
+            }
+        }
+    }
+}
